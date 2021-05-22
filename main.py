99668ebe60b2ad8dc79ff74552f8cc34795517e9
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-05-22 18:45:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ starpreview = {}
 for star in stars:
     starpreview.update({i : star})
     i=i+1
-print("\n".join("{}\t{}".format(key, value.encode('utf-8')) for key, value in starpreview.items()))
+print("\n".join("{}\t{}".format(key, value) for key, value in starpreview.items()))
 
 vehicles = TravelVehicle.vehicle_dict.keys()
 vehiclepreview = {}
@@ -29,20 +29,9 @@ for vehicle in vehicles:
     n=n+1
 
 destination = input("Enter stars index number: ")
-print("\n".join("{}\t{}".format(key, value.encode('utf-8')) for key, value in vehiclepreview .items()))
+print("\n".join("{}\t{}".format(key, value) for key, value in vehiclepreview .items()))
 transport = input("select transportation (type serial number):")
 
 travel_hour = StarSystem.star_data[starpreview[int(destination)]]['distance_ly'] * constants.one_lightyear_km/TravelVehicle.vehicle_dict[vehiclepreview[int(transport)]]['Speed (km/h)']
 
 print('\nThe journey to ' + starpreview[int(destination)] + ' by ' + vehiclepreview[int(transport)] + ' will take '+ str((travel_hour/24)/365) + ' years\n')
-
-
-
-
-
-
-
-
-
-
-

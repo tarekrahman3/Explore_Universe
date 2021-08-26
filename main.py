ae5cd5cb8c5530a7fb1d0b48a1ebb4572b125cf5
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-08-27 04:07:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 
-class constants():
+class Constants():
     one_lightyear_km = float(9.46) * pow(10, 12)
     Mile_to_Kilo = float(1.60934)
     Kilo_to_Mile = float(0.621371)
@@ -13,25 +13,39 @@ class TravelVehicle():
     with open('list of vehicles.json', 'r') as json_v:
         vehicle_dict = json.load(json_v)
 
-stars = StarSystem.star_data.keys()
-i = 0
-starpreview = {}
-for star in stars:
-    starpreview.update({i : star})
-    i=i+1
-print("\n".join("{}\t{}".format(key, value) for key, value in starpreview.items()))
+class DataViewer():
+    def show_all_stars():
+        stars = StarSystem.star_data.keys()
+        i = 0
+        starpreview = {}
+        for star in stars:
+            starpreview.update({i : star})
+            i=i+1
+        print("\n".join("{}\t{}".format(key, value) for key, value in starpreview.items()))
+        return starpreview
 
-vehicles = TravelVehicle.vehicle_dict.keys()
-vehiclepreview = {}
-n = 0
-for vehicle in vehicles:
-    vehiclepreview.update({n : vehicle})
-    n=n+1
+    def show_all_vehicles():
+        vehicles = TravelVehicle.vehicle_dict.keys()
+        vehiclepreview = {}
+        n = 0
+        for vehicle in vehicles:
+            vehiclepreview.update({n : vehicle})
+            n=n+1
+        print("\n".join("{}\t{}".format(key, value) for key, value in vehiclepreview .items()))
+        return vehiclepreview
 
-destination = input("Enter stars index number: ")
-print("\n".join("{}\t{}".format(key, value) for key, value in vehiclepreview .items()))
-transport = input("select transportation (type serial number):")
+def take_user_inputs():
+    starpreview = DataViewer.show_all_stars()
+    destination = input("Enter stars index number: ")
+    vehiclepreview = DataViewer.show_all_vehicles()
+    transport = input("select transportation (type serial number):")
+    return (destination, transport, starpreview, vehiclepreview)
 
-travel_hour = StarSystem.star_data[starpreview[int(destination)]]['distance_ly'] * constants.one_lightyear_km/TravelVehicle.vehicle_dict[vehiclepreview[int(transport)]]['Speed (km/h)']
+def calculate_the_journey_time(destination, transport):
+    travel_hour = StarSystem.star_data[starpreview[int(destination)]]['distance_ly'] * Constants.one_lightyear_km/TravelVehicle.vehicle_dict[vehiclepreview[int(transport)]]['Speed (km/h)']
+    return travel_hour
 
-print('\nThe journey to ' + starpreview[int(destination)] + ' by ' + vehiclepreview[int(transport)] + ' will take '+ str((travel_hour/24)/365) + ' years\n')
+if __name__ =='__main__':
+    destination, transport, starpreview, vehiclepreview = take_user_inputs()
+    travel_hour = calculate_the_journey_time(destination, transport)
+    print('\nThe journey to ' + starpreview[int(destination)] + ' by ' + vehiclepreview[int(transport)] + ' will take '+ str((travel_hour/24)/365) + ' years\n')
